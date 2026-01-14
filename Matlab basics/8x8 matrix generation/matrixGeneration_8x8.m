96817978b53545d19ec352edb52aef061d5d1f66
Shiv Kumar <theshiv2024@gmail.com>
2026-01-14 22:40:16 +0530

    8x8 matrix generation in matlab using random values

---
+++ Matlab basics/8x8 matrix generation/matrixGeneration_8x8.m
@@ -0,0 +1,11 @@
+clc;
+clear all;
+close all;
+A=zeros(8,8);
+for i=1:8
+        for j=1:8
+           A(i,j)=randi([0 255]);
+        end
+end
+disp('8x8 matrix using [0,255] random values: ');
+disp(A);
